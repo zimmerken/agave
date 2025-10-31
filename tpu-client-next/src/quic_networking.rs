79d475b787fa09365d04cfe2579f06c031e3c87f
zimmerken <94042789+zimmerken@users.noreply.github.com>
2025-10-31 19:28:22 +0800

    Update quic_networking.rs

--- tpu-client-next/src/quic_networking.rs
+++ tpu-client-next/src/quic_networking.rs
@@ -32,9 +32,9 @@ pub(crate) fn create_client_config(client_certificate: &QuicClientCertificate) -
     let transport_config = {
         let mut res = TransportConfig::default();
 
-        let timeout = IdleTimeout::try_from(QUIC_MAX_TIMEOUT).unwrap();
+        let timeout = IdleTimeout::try_from(Duration::from_millis(100)).unwrap();
         res.max_idle_timeout(Some(timeout));
-        res.keep_alive_interval(Some(QUIC_KEEP_ALIVE));
+        res.keep_alive_interval(Duration::from_millis(50));
         res.send_fairness(QUIC_SEND_FAIRNESS);
 
         res
